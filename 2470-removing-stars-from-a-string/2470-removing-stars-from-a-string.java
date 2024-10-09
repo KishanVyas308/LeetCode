b04d8e35c86a56cf5f50b1c65c49e6871094947d
Kishan Vyas <kishanvyas308@gmail.com>
2024-10-09 19:48:45 +0530

    Time: 30 ms (91.13%), Space: 45.3 MB (91.01%) - LeetHub

---
+++ 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String removeStars(String s) {
+        StringBuilder str = new StringBuilder();
+        for(char ch : s.toCharArray()) {
+            if(ch != '*') {
+                str.append(ch);
+            }
+            else {
+                str.deleteCharAt(str.length()-1);
+            }
+        }
+        return str.toString();
+    }
+}
